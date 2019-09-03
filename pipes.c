b866dd629f020bee309068c00a3b20b5647bb77b
John Pettigrew <john.pettigrew404@gmail.com>
2019-09-02 22:11:35 -0500

    start work on moving pipes

--- pipes.c
+++ pipes.c
@@ -1,5 +1,8 @@
+#define PIPE_DATA_SIZE 56
+
 struct pipe {
 	int tileStart;
 	int x;
 	int y;
+	unsigned char pipeData[PIPE_DATA_SIZE];
 };
--- test01.c
+++ test01.c
@@ -1,21 +1,27 @@
 #include <stdio.h>
+#include <rand.h>
+#include <time.h>
 #include "sprites.c"
 #include "pipes.c"
 #include <gb/gb.h>
 
 int gravity = 4;
-int jumpPower = 25;
+int jumpPower = 30;
 int playerPosX = 30;
 int playerPosY = 60;
 int lastAStatus = 0;
 int currentPlayerFrame = 0;
 
-const int numPipes = 2;
-struct pipe pipes[2];
-int pipeDistance = 125;
-int pipeSpawnStart = 20;
-int pipeSpeed = 3;
-int i, j, k, l, m;
+const int BOTTOM_PIPE_TILE = 8;
+const int MID_PIPE_TILE = 10;
+const int TOP_PIPE_TILE = 10;
+
+#define NUM_PIPES 2
+struct pipe pipes[NUM_PIPES];
+int pipeDistanceX = 16;
+int pipeSpawnStart = 16;
+int pipeSpeed = 2;
+int i, j, k, l, m, n, o;
 
 void applyInputForce(){
 	if(joypad() == J_A){
@@ -53,48 +59,95 @@ void drawCharacter(){
 	move_sprite(1, playerPosX + 8, playerPosY);
 }
 
-movePipes(){
-	for(i = 0; i < numPipes; i++){
-		pipes[i].x = pipes[i].x - pipeSpeed;
+void initPipes(){
+	for(j = 0; j < NUM_PIPES; j++){
+		pipes[j].x = pipeSpawnStart + (pipeDistanceX * j);
+		pipes[j].y = 70;
+		pipes[j].tileStart = 2 + (j * 20);
+
+		//bottom pipe
+		set_sprite_tile(pipes[j].tileStart, BOTTOM_PIPE_TILE);
+		set_sprite_tile(pipes[j].tileStart + 1, BOTTOM_PIPE_TILE + 1);
+
+		//pipe
+		set_sprite_tile(pipes[j].tileStart + 2, MID_PIPE_TILE);
+		set_sprite_tile(pipes[j].tileStart + 3, MID_PIPE_TILE + 1);
+
+		//top pipe
+		set_sprite_tile(pipes[j].tileStart + 4, TOP_PIPE_TILE);
+		set_sprite_tile(pipes[j].tileStart + 5, TOP_PIPE_TILE + 1);
 	}
 }
 
-void initPipes(){
-	for(j = 0; j < numPipes; j++){
-		pipes[j].x = pipeSpawnStart + (pipeDistance * j);
-		pipes[j].y = 70;
-		pipes[j].tileStart = 2 + (j * 16);
+void drawPipe(int pipeNum){
+	set_bkg_tiles(pipes[pipeNum].x, pipes[pipeNum].y, 2, 28, pipes[pipeNum].pipeData);
+}
 
-		//Pipe top
-		set_sprite_tile(pipes[j].tileStart, 8);
-		set_sprite_tile(pipes[j].tileStart + 1, 10);
+void clearPipe(int pipeNum){
+	set_bkg_tiles(pipes[pipeNum].x, pipes[pipeNum].y, 2, 28, ClearPipeBkgData);
+}
 
-		for(k = 0; k <= 12; k+=2){
-			//Pipe bottom
-			set_sprite_tile(pipes[j].tileStart + 2 + k, 12);
-			set_sprite_tile(pipes[j].tileStart + 3 + k, 14);
+void clearBackground(){
+	set_bkg_tiles(0, 0, 32, 32, ClearBkgData);
+}
+
+void drawAllPipes(){
+	clearBackground();
+	for(m = 0; m < NUM_PIPES; m++){
+		drawPipe(m);
+	}
+}
+
+int pipeToAssign = 0;
+void setPipeData(int pipePos){
+	for(n = 0; n < 56; n+=4){
+		/*
+		//pipe top opening
+		if(n == pipes[pipePos].y - 8){
+			pipeToAssign = 0;
+		}
+		//blank
+		else if(n == pipes[pipePos].y - 4){
+			pipeToAssign = 1;
+		}
+		else if(n == pipes[pipePos].y){
+			pipeToAssign = 1;
+		}
+		else if(n == pipes[pipePos].y + 4){
+			pipeToAssign = 1;
+		}
+		//pipe bottom opening
+		else if(n == pipes[pipePos].y + 8){
+			pipeToAssign = 2;
+		}
+		//regular pipe
+		else if(n < pipes[pipePos].y){
+			pipeToAssign = 3;
+		}
+		else if(n > pipes[pipePos].y){
+			pipeToAssign = 3;
+		}
+		*/
+
+		for(o = 0; o < 4; o++){
+			//pipes[pipePos].pipeData[n + o] = pipeTileSets[pipeToAssign][o];
+			pipes[pipePos].pipeData[n + o] = 0x0f;
 		}
 	}
 }
 
-int pipeStartY;
-void drawPipe(int pipeNum){
-	//Pipe top
-	move_sprite(pipes[pipeNum].tileStart, pipes[pipeNum].x, pipes[pipeNum].y);
-	move_sprite(pipes[pipeNum].tileStart + 1, pipes[pipeNum].x+8, pipes[pipeNum].y);
-	pipeStartY = pipes[pipeNum].y + 8;
-
-	for(l = 0; l <= 12; l+=2){
-		//Pipe bottom
-		move_sprite(pipes[pipeNum].tileStart + 2 + l, pipes[pipeNum].x, pipeStartY+(8*l));
-		move_sprite(pipes[pipeNum].tileStart + 3 + l, pipes[pipeNum].x+8, pipeStartY+(8*l));
+void movePipes(){
+	for(i = 0; i < NUM_PIPES; i++){
+		pipes[i].x = pipes[i].x - pipeSpeed;
+		if(pipes[i].x <= 0){
+			clearPipe(i);
+			pipes[i].x = 34;
+			pipes[i].y = ((rand() % (20 - 8 + 1)) + 8) - 16;
+			setPipeData(i);
+			drawPipe(i);
+		}
 	}
-}
-
-void drawAllPipes(){
-	for(m = 0; m < numPipes; m++){
-		drawPipe(m);
-	}
+	scroll_bkg(pipeSpeed, 0);
 }
 
 void update(){
@@ -105,24 +158,30 @@ void update(){
 	drawCharacter();
 
 	movePipes();
-	drawAllPipes();
 }
 
 void main(){
+	//srand(time(0));
+
 	SPRITES_8x16;
+	DISPLAY_ON;
 	set_sprite_data(0, 8, BirdData);
 	set_sprite_tile(0, 0);
 	set_sprite_tile(1, 2);
 
-	set_sprite_data(8, 8, PipesData);
-
 	initPipes();
 
+	set_bkg_data(0, 64, PipesData);
+
+	drawAllPipes();
+
+	HIDE_WIN;
 	SHOW_SPRITES;
+	SHOW_BKG;
 
 	while(1){
 		update();
-		delay(50);
+		delay(40);
 	}
 }
 
